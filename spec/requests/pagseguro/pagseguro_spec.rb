edc8d2158ee2c44468a23de16ba2ce1aedb64550
Tiago Lima <tiago.alves@koinz.com.br>
2021-02-07 12:04:41 -0300

    inprogress#develop - rspec responds with session id present

--- spec/requests/pagseguro/pagseguro_spec.rb
+++ spec/requests/pagseguro/pagseguro_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Pagseguro', type: :request do
-  describe 'GET /session' do
-    it 'works! (now write some real specs)' do
-      # get users_index_path
-      # expect(response).to have_http_status(200)
+  let(:url_base) { 'https://ws.sandbox.pagseguro.uol.com.br/v2' }
+  let(:auth) { auth_params }
+
+  describe 'Sandbox Pagseguro integration' do
+    context 'POST /sessions' do
+      it 'responds with session id present' do
+        response = HTTParty.post("#{url_base}/sessions?email=#{auth[:email]}&token=#{auth[:token]}")
+        expect(response['session']['id'].present?).to eq(true)
+      end
     end
+  end
+
+  def auth_params
+    {
+      email: Rails.application.credentials[Rails.env.to_sym][:email],
+      token: Rails.application.credentials[Rails.env.to_sym][:pag_seguro_test_token]
+    }
   end
 end
