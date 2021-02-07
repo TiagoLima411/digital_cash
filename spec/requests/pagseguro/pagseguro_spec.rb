f16707f82084ba48fcd8d1dd7758bafbf53f7afd
Tiago Lima <tiago.alves@koinz.com.br>
2021-02-07 12:43:26 -0300

    inprogress#develop - responds with session Unauthorized

--- spec/requests/pagseguro/pagseguro_spec.rb
+++ spec/requests/pagseguro/pagseguro_spec.rb
@@ -10,13 +10,20 @@ RSpec.describe 'Pagseguro', type: :request do
         response = HTTParty.post("#{url_base}/sessions?email=#{auth[:email]}&token=#{auth[:token]}")
         expect(response['session']['id'].present?).to eq(true)
       end
+
+      it 'responds with session Unauthorized' do
+        response = HTTParty.post("#{url_base}/sessions?email=#{auth[:fake_email]}&token=#{auth[:fake_token]}")
+        expect(response.body).to eq('Unauthorized')
+      end
     end
   end
 
   def auth_params
     {
       email: Rails.application.credentials[Rails.env.to_sym][:email],
-      token: Rails.application.credentials[Rails.env.to_sym][:pag_seguro_test_token]
+      token: Rails.application.credentials[Rails.env.to_sym][:pag_seguro_test_token],
+      fake_email: Faker::Internet.email,
+      fake_token: Faker::Alphanumeric.alpha(32).upcase
     }
   end
 end
