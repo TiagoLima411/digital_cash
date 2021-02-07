83cdbe8b39d5b0a12b52e4f5c005c42f583afcd2
Tiago Lima <tiago.alves@koinz.com.br>
2021-02-06 22:44:06 -0300

    Test started with Pagseguro api.

---
+++ spec/requests/pagseguro/pagseguro_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Pagseguro', type: :request do
+  describe 'GET /session' do
+    it 'works! (now write some real specs)' do
+      # get users_index_path
+      # expect(response).to have_http_status(200)
+    end
+  end
+end
